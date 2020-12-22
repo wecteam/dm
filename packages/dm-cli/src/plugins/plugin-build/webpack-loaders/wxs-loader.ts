18a1cf17007d71ebcb77cc7c7c50ea6335471ff7
chenxiaoqiang12 <chenxiaoqiang@jd.com>
2020-12-22 20:23:35 +0800

    fix(build): wxs文件内引用另一个wxs文件无法分析到依赖的bug

--- packages/dm-cli/src/plugins/plugin-build/webpack-loaders/wxs-loader.ts
+++ packages/dm-cli/src/plugins/plugin-build/webpack-loaders/wxs-loader.ts
@@ -2,5 +2,5 @@ import { IWebpack } from '../../../interfaces';
 
 export = function (this: IWebpack.loader.LoaderContext, content: string): string {
   this.cacheable && this.cacheable();
-  return JSON.stringify(content);
+  return content;
 }
